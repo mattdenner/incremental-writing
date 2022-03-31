1e515037e8b110e0285cf30b873240d025afb1ff
Matthew Denner <matthew.denner@hopin.to>
2022-03-31 09:27:51 +0100

    FIX: roll-up

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,10 @@
+var { nodeResolve } = require('@rollup/plugin-node-resolve');
+
 module.exports = (config, context) => {
   config.output.exports = 'default';
   config.output.sourcemap = 'inline';
+
+  config.plugins = config.plugins || [];
+  config.plugins.push(nodeREsolve());
   return config;
 };
