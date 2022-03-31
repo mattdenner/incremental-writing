23473d29558e0c5a163a3da5822f8ad791baaba5
Matthew Denner <matthew.denner@hopin.to>
2022-03-31 10:28:13 +0100

    FIX: rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,6 @@
-var { nodeResolve } = require('@rollup/plugin-node-resolve');
-
 module.exports = (config, context) => {
   config.output.exports = 'default';
   config.output.sourcemap = 'inline';
-
-  config.plugins = config.plugins || [];
-  config.plugins.push(nodeREsolve());
+  config.external = ['obsidian'];
   return config;
 };
